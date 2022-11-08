aecafb88c933ca9a6d2266884fac89c8bb1b0232
elminhoemmanuel <amodue15@gmail.com>
2022-11-09 00:23:55 +0100

    setup dark mode toggle

---
+++ components/page-elements/TopNavbar/TopNavbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import css from './topNavbar.module.css'
+
+const TopNavbar = () => {
+  return (
+    <div>TopNavbar</div>
+  )
+}
+
+export default TopNavbar
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,34 @@
 import Head from 'next/head'
+import { useState, useEffect } from 'react';
+import css from '@/styles/home.module.css';
+import TopNavbar from '@/components/page-elements/TopNavbar';
 
 export default function Home() {
-  return (
-    <div >
-      <Head>
-        <title>aguythatcodes | Frontend Developer</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    const [theme, setTheme] = useState<string | null>("dark");
 
-      <div className='2xl:max-w-screen-2xl 2xl:mx-auto w-full text-red-500 text-sm relative'>
-         Hello
-      </div>
-      
-    </div>
-  )
+    const toggleTheme = () => {
+        const newTheme = theme === "dark" ? "light" : "dark";
+        setTheme(newTheme);
+        localStorage.setItem("theme", newTheme);
+    }
+
+    useEffect(() => {
+        const existingTheme = localStorage.getItem("theme");
+        setTheme(existingTheme);
+    }, [])
+
+    return (
+        <div >
+            <Head>
+                <title>aguythatcodes | Frontend Developer</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <div data-theme={theme} onClick={toggleTheme} className={css.container}>
+                <TopNavbar />
+                Hello John
+            </div>
+
+        </div>
+    )
 }

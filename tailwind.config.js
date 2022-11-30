5ced970d008d1e9e4fb71a68a5f5dc927f19c116
elminhoemmanuel <amodue15@gmail.com>
2022-11-30 20:42:10 +0100

    background with blur

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
             'sm': '640px',
             // => @media (min-width: 640px) { ... }
 
-            'md': '768px',
+            'md': '769px',
             // => @media (min-width: 768px) { ... }
 
             'lg': '1025px',
